a4bc21a1e24b0ac86564b1259a2583ed680a29e2
Mike Boers <github@mikeboers.com>
2014-01-28 14:53:16 -0800

    Reduce pulls initial from args

--- aque/handlers/reduce.py
+++ aque/handlers/reduce.py
@@ -9,11 +9,13 @@ log = logging.getLogger(__name__)
 def reduce_children(job):
 
     func = decode_callable(job.get('func'))
+
+    args = job.get('args', ())
+    if len(args) >= 2:
+        job.error('too many args; reduce expects 1, got %d' % len(args))
+        return
+
     sequence = [child.result() for child in job.children()]
-    try:
-        args = (job['initial'], )
-    except:
-        args = ()
 
     log.debug('reducing %r with %r and %r' % (sequence, func, args))
     
