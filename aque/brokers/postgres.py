af1d2be9706b5ee7319d9ad8b401122b3402c0cd
Mike Boers <github@mikeboers.com>
2014-03-10 11:27:02 -0700

    Tests pass with PostgresBroker

--- aque/brokers/postgres.py
+++ aque/brokers/postgres.py
@@ -23,6 +23,18 @@ class literal(str):
 
 class PostgresBroker(Broker):
 
+    _fields = (
+        'id',
+        'status',
+        'priority',
+        'user',
+        'group',
+        'pattern',
+        'func',
+        'args',
+        'kwargs',
+    )
+
     @classmethod
     def from_url(cls, parts):
         return cls(database=parts.path.strip('/').lower())
@@ -30,7 +42,7 @@ class PostgresBroker(Broker):
     def __init__(self, **kwargs):
         super(PostgresBroker, self).__init__()
 
-        self._dbname = kwargs['database']
+        self._kwargs = kwargs
         self._pool = kwargs.pop('pool', None)
         if self._pool is None:
             self._pool = pg.pool.ThreadedConnectionPool(0, 10, **kwargs)
@@ -54,15 +66,30 @@ class PostgresBroker(Broker):
         with self._cursor() as cur:
             cur.execute('''CREATE TABLE IF NOT EXISTS tasks (
                 id SERIAL PRIMARY KEY,
-                status TEXT NOT NULL
+                status TEXT,
+                priority INTEGER,
+                "user" TEXT,
+                "group" TEXT,
+                pattern TEXT,
+                func TEXT,
+                args TEXT,
+                kwargs TEXT
+            )''')
+            cur.execute('''CREATE TABLE IF NOT EXISTS dependencies (
+                id SERIAL PRIMARY KEY,
+                depender INTEGER references tasks(id),
+                dependee INTEGER references tasks(id)
             )''')
 
     def clear(self):
-        with pg2.connect(database='postgres') as conn:
+        dbname = self._kwargs['database']
+        kwargs = self._kwargs.copy()
+        kwargs['database'] = 'postgres'
+        with pg.connect(**kwargs) as conn:
             conn.set_isolation_level(0)
             with conn.cursor() as cur:
-                cur.execute('DROP DATABASE IF EXISTS %s' % self._dbname)
-                cur.execute('CREATE DATABASE %s' % self._dbname)
+                cur.execute('DROP DATABASE IF EXISTS %s' % dbname)
+                cur.execute('CREATE DATABASE %s' % dbname)
 
     def create(self, prototype=None):
         with self._cursor() as cur:
@@ -73,10 +100,42 @@ class PostgresBroker(Broker):
         return self.get_future(tid)
 
     def fetch(self, tid):
-        raise NotImplementedError()
+        query = 'SELECT %s FROM tasks WHERE id = %%s' % ', '.join('"%s"' % name for name in self._fields)
+        with self._cursor() as cur:
+            cur.execute(query, (tid, ))
+            row = cur.fetchone()
+            task = dict(zip(self._fields, row))
+            cur.execute('SELECT dependee FROM dependencies WHERE depender = %s', (tid, ))
+            task['dependencies'] = [row[0] for row in cur]
+        return task
 
     def update(self, tid, data):
-        pass
+        fields = []
+        params = []
+        for name in self._fields:
+            try:
+                value = data.pop(name)
+            except KeyError:
+                pass
+            else:
+                fields.append(name)
+                params.append(utils.encode_if_required(value))
+
+        deps = data.pop('dependencies', None)
+
+        if data:
+            raise ValueError('unexpected keys: %s' % ', '.join(sorted(data)))
+
+        params.append(tid)
+        query = 'UPDATE tasks SET %s WHERE id = %%s' % ', '.join('"%s" = %%s' % name for name in fields)
+        with self._cursor() as cur:
+            cur.execute(query, params)
+            if deps is not None:
+                cur.execute('DELETE FROM dependencies WHERE depender = %s', (tid, ))
+                cur.executemany(
+                    'INSERT INTO dependencies(depender, dependee) VALUES(%s, %s)',
+                    [(tid, dep) for dep in deps]
+                )
 
     def set_status_and_notify(self, tid, status):
         with self._cursor() as cur:
