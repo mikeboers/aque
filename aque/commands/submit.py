58db2ca95bce8b170edcbcb3fe1d179e00908ba8
Mike Boers <github@mikeboers.com>
2014-03-11 16:52:17 -0700

    `aque submit -w` can do basic waiting for tasks

--- aque/commands/submit.py
+++ aque/commands/submit.py
@@ -4,14 +4,23 @@ from aque.commands.main import command, argument
 
 
 @command(
-    argument('command'),
-    argument('args', nargs='*'),
+    argument('-w', '--wait', action='store_true'),
+    argument('-t', '--timeout', type=float),
+    argument('command', nargs='+'),
     help='schedule a shell command',
     aliases=['s', 'sub'],
 )
 def submit(args):
-    cmd = [args.command] + args.args
+    cmd = args.command
     if args.verbose:
         print >> sys.stderr, cmd
     future = args.queue.submit_ex(pattern='shell', args=cmd)
-    print future.id
+    if args.wait:
+        try:
+            res = future.result(args.timeout)
+        except Exception as e:
+            print e.__class__.__name__, e
+        else:
+            print repr(res)
+    else:
+        print future.id
